bb90ab107a2132fac0ec00ec1becfcf3b49f12d7
julfikar092 <julfikar.iitdu@gmail.com>
2026-01-18 01:34:40 +0600

    Two D list and list comprehension

---
+++ Innovative Skills/two_dimentional_list.py
@@ -0,0 +1,11 @@
+marks_of_class = [[89,92,75,94,95], [77,89,65,79,77,65,41], [55,98,68,35,74,88]]
+
+# for row in range(len(marks_of_class)):
+#     for col in range (len(marks_of_class[row])):
+#         print(marks_of_class[row][col])
+
+
+for row in marks_of_class:
+   
+    for col in row:
+        print(col)
